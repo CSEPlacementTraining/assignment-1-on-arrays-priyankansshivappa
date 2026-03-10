f5e622e87fb7919632d5dce65ca0df6a7abe6dba
priyankansshivappa <gpriyankans@gmail.com>
2026-03-10 21:50:39 +0530

    Solution for Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
-public class Assignment1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        int count = 0;
-        while (count < 15) {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.print(year + " ");
-                count++;
-            }
-            year++;
-        }
+class Assignment1 {
+  public static int[] findLeapYears(int year) {
+    int[] leapYears = new int[15];
+    int count = 0;
+    while (count < 15) {
+      if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        leapYears[count] = year;
+        count++;
+      }
+      year++;
     }
+    return leapYears;
+  }
+  public static void main(String[] args) {
+    int year = 2000;
+    int[] leapYears;
+    leapYears = findLeapYears(year);
+    for (int index = 0; index < leapYears.length; index++) {
+      System.out.println(leapYears[index]);
+    }
+  }
 }
-
